e4cbf54bccd54e203039a6f105834a5bcd9ce88f
firehellrain <firehellrain@gmail.com>
2022-06-09 11:15:53 +0200

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,27 +1,40 @@
 import geneticalgorithm.GeneticAlgorithm;
 import com.qqwing.QQWing;
 
-
 import java.util.Arrays;
 
 public class App {
 
     public static void main(String[] args) {
-        QQWing mySudoku = new QQWing();
-        mySudoku.setPuzzle(new int[] {0, 5, 0, 7, 6, 3, 1, 9, 0, 0, 0, 0, 1, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 1, 0, 4, 0, 8, 0, 0, 0, 3, 0, 0, 6, 0, 0, 4, 0, 7, 9, 0, 0, 0, 1, 0, 0, 0, 0, 5, 3, 0, 0, 0, 0, 9, 7, 0, 4, 0, 0, 0, 0, 0, 8, 0, 0, 0, 0, 0
+        
+        QQWing sudoku = new QQWing();
+        
+        sudoku.setPuzzle(new int[] {
+            1, 9, 0, 0, 2, 0, 5, 0, 8,
+            0, 6, 7, 0, 0, 0, 0, 4, 0,
+            0, 0, 4, 6, 8, 3, 0, 9, 0,
+            3, 0, 0, 7, 0, 0, 2, 0, 9, 
+            0, 0, 0, 1, 0, 0, 6, 0, 5, 
+            0, 0, 0, 5, 9, 8, 0, 0, 4, 
+            4, 0, 5, 8, 0, 0, 9, 0, 6, 
+            2, 0, 6, 0, 4, 0, 0, 5, 1, 
+            9, 0, 1, 0, 0, 6, 0, 7, 0, 
         });
 
-        int[] sudoku = mySudoku.getPuzzle();
+        int[] sdkAsArray = sudoku.getPuzzle();
 
-        Arrays.stream(sudoku).forEach(c -> System.out.print(" " + c));
+        Arrays.stream(sdkAsArray).forEach(c -> System.out.print(" " + c));
         System.out.println();
-        mySudoku.printPuzzle();
-        GeneticAlgorithm algorithm = new GeneticAlgorithm(sudoku);
+        sudoku.printPuzzle();
+
+        GeneticAlgorithm algorithm = new GeneticAlgorithm(sdkAsArray);
+
         System.out.println("Solution:");
-        mySudoku.solve();
-        mySudoku.printSolution();
+        sudoku.solve();
+        sudoku.printSolution();
+        
         int[] sol = algorithm.solve();
-        mySudoku.setPuzzle(sol);
-        mySudoku.printPuzzle();
+        sudoku.setPuzzle(sol);
+        sudoku.printPuzzle();
     }
 }
